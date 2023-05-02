8c22b12656ed504fdfbe36a37664cafbdd14aa54
oleGAPrelev <101264107+oleGAPrelev@users.noreply.github.com>
2023-05-02 09:10:56 +0200

    working with API, http

--- lib/features/crypto_list/view/crypto_list_screan.dart
+++ lib/features/crypto_list/view/crypto_list_screan.dart
@@ -1,4 +1,6 @@
 import 'package:crypto_coins_list/features/crypto_list/wigets/wigets.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins_repository.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/modals/crypto_coin.dart';
 import 'package:flutter/material.dart';
 
 class CryptoListScreen extends StatefulWidget {
@@ -11,19 +13,39 @@ class CryptoListScreen extends StatefulWidget {
 }
 
 class _CryptoListScreenState extends State<CryptoListScreen> {
+  List<CryptoCoin>? _cryptoCoinsList;
+
+  @override
+  void initState() {
+    _loadCryptoCoins();
+    super.initState();
+  }
+
+  // @override
+  // void initState() {
+  //   CryptoCoinsRepository().getCoinsList();
+  //   super.initState();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CryptoCurrenciesList'),
       ),
-      body: ListView.separated(
-          itemCount: 10,
-          separatorBuilder: (context, index) => const Divider(),
-          itemBuilder: (context, i) {
-            const coinName = 'BItcoin';
-            return const CryptoCoinTile(coinName: coinName);
-          }),
+      body: (_cryptoCoinsList == null)
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.separated(
+              padding: const EdgeInsets.only(top: 16),
+              itemCount: _cryptoCoinsList!.length,
+              separatorBuilder: (context, index) => const Divider(),
+              itemBuilder: (context, i) {
+                final coin = _cryptoCoinsList![i];
+                return CryptoCoinTile(coin: coin);
+              }),
     );
   }
+
+  Future<List<CryptoCoin>> _loadCryptoCoins() async =>
+      _cryptoCoinsList = await CryptoCoinsRepository().getCoinsList();
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,6 +5,8 @@ final darkThem = ThemeData(
   primarySwatch: Colors.yellow,
   dividerColor: Colors.white24,
   appBarTheme: const AppBarTheme(
+    iconTheme: IconThemeData(color: Colors.white),
+    elevation: 0,
     backgroundColor: Color.fromARGB(255, 31, 31, 31),
     titleTextStyle: TextStyle(
       color: Colors.white,
